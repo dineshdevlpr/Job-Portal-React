2f87487dfb0dc2734d92436fb9e2fae313ef7bda
dineshdevlpr <dineshiaf30@gmail.com>
2021-12-03 01:55:56 +0530

    Update Route Fixed

--- src/components/jobsPortal/recruiterPortal/updatejob.js
+++ src/components/jobsPortal/recruiterPortal/updatejob.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useHistory } from "react-router-dom";
 import loader from '../../../loader.svg'
 
 
 export default function UpdateJob(props) {
 
+    const[jobData, setJobData] = useState([])
     const [jobId, setJobId] = useState("");
     const [description, setDescription] = useState("");
     const [skills, setSkills] = useState("");
@@ -17,6 +18,27 @@ export default function UpdateJob(props) {
         alert("No Token Found. Try Log in.")
         history.push("/recruiter/login")
     }
+
+    //get job data to show in the update page
+    useEffect( () => {
+        async function fetchData() {
+        let fetchData = await fetch(`https://jobportaltask.herokuapp.com/recruiter-portal/updatejob/${id}`, {
+          method: "GET",
+          mode: 'cors',
+          headers: {
+            "Content-type": "application/json",
+            "Authorization" : authToken
+          },
+        })
+        let job = await fetchData.json();
+       
+        setJobData(job);
+        console.log(jobData)
+        setLoading(false)
+    }
+    fetchData();
+    }, [])
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -56,15 +78,15 @@ export default function UpdateJob(props) {
                                 <h3 className="text-center text-info">Update Job</h3>
                                 <div className="form-group">
                                     <label for="jobId" className="text-info">Enter Job ID</label><br/>
-                                    <input type="text" id="jobId" name="jobId" required onChange={(e) => setJobId(e.target.value)} className="form-control" />
+                                    <input type="text" id="jobId" name="jobId" defaultValue={jobData.jobId} required onChange={(e) => setJobId(e.target.value)} className="form-control" />
                                 </div>
                                 <div className="form-group">
                                     <label for="description" className="text-info">Enter Description</label><br/>
-                                    <input type="text" id="description" name="description" required onChange={(e) => setDescription(e.target.value)} className="form-control" />
+                                    <input type="text" id="description" name="description" defaultValue={jobData.description} required onChange={(e) => setDescription(e.target.value)} className="form-control" />
                                 </div>
                                 <div className="form-group">
                                     <label for="skills" className="text-info">Enter Skills Required</label><br/>
-                                    <input type="text" id="skills" name="skills" required onChange={(e) => setSkills(e.target.value)} className="form-control" />
+                                    <input type="text" id="skills" name="skills" defaultValue={jobData.skills} required onChange={(e) => setSkills(e.target.value)} className="form-control" />
                                 </div>
                                 <div className="form-group">
                                     <input type="submit" name="submit" className="btn btn-info btn-md" value="submit" />
